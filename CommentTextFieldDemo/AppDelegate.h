b53584b4f8c877b4f985ccf0f5442e898e5c448c
yuyedaidao <wyqpadding@gmail.com>
2015-04-16 20:39:19 +0800

    Initial Commit

---
+++ CommentTextFieldDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CommentTextFieldDemo
+//
+//  Created by Wang on 15/4/16.
+//  Copyright (c) 2015年 Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
